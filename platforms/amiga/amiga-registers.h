38919241d82789ff6e7e81474f1922fecf57d309
Adam Polkosnik <apolkosnik@gmail.com>
2021-01-03 01:37:10 -0500

    - Added support for A4000 IDE on A4000 ROMs to boot the hd0.img - Added some RAMSEY and GARY stuff to make things usable on 68030+ and A4000 ROMs

--- platforms/amiga/amiga-registers.h
+++ platforms/amiga/amiga-registers.h
@@ -1,2 +1,17 @@
 void configure_rtc_emulation_amiga(uint8_t enabled);
 void set_hard_drive_image_file_amiga(uint8_t index, char *filename);
+
+/* GARY ADDRESSES */
+#define GARY_REG0 0xDE0000
+#define GARY_REG1 0xDE0001
+#define GARY_REG2 0xDE0002
+#define GARY_REG3 0xDE1000
+#define GARY_REG4 0xDE1001
+#define GARY_REG5 0xDE1002
+
+/* RAMSEY ADDRESSES */
+#define RAMSEY_REG 0xDE0003 /* just a nibble, it should return 0x08 for defaults with 16MB */
+#define RAMSEY_ID 0xDE0043  /* Either 0x0D or 0x0F (most recent version) */
+/* RAMSEY TYPES */
+#define RAMSEY_REV4 0x0D
+#define RAMSEY_REV7 0x0F
