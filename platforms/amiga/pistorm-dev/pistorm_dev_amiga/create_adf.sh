65aaff6b83af21db3a80fbfddd433fbbb2efdad6
Andrew Hutchings <andrew@linuxjedi.co.uk>
2021-05-10 16:11:00 +0100

    Add GUI generation scripts for ADF and LHA

---
+++ platforms/amiga/pistorm-dev/pistorm_dev_amiga/create_adf.sh
@@ -0,0 +1,10 @@
+# Requires xdftool from amitools (https://github.com/cnvogelg/amitools/)
+xdftool pistorm.adf create
+xdftool pistorm.adf format "PiStorm"
+xdftool pistorm.adf write PiSimple
+xdftool pistorm.adf write PiStorm
+xdftool pistorm.adf write PiStorm.info
+xdftool pistorm.adf write libs13
+xdftool pistorm.adf write libs20
+xdftool pistorm.adf write libs13.info
+xdftool pistorm.adf write libs20.info
