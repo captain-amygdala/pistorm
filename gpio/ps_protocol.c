cc0244c6090839c95e4a4534b5a608c2f4910997
captain-amygdala <claude.schwarz@gmail.com>
2023-12-12 11:11:38 +0100

    Revert MDIO FIX, needs further checking first

--- gpio/ps_protocol.c
+++ gpio/ps_protocol.c
@@ -34,11 +34,11 @@ extern void m68k_pulse_bus_error(void);
 
 #define GPFSEL0_INPUT (GO(PIN_WR) | GO(PIN_RD))
 #define GPFSEL1_INPUT (0)
-#define GPFSEL2_INPUT (GO(PIN_A(2)) | GO(PIN_A(1)) | GO(PIN_A(0)) | PF(28,5) | PF(29,5))
+#define GPFSEL2_INPUT (GO(PIN_A(2)) | GO(PIN_A(1)) | GO(PIN_A(0)))
 
 #define GPFSEL0_OUTPUT (GO(PIN_D(1)) | GO(PIN_D(0)) | GO(PIN_WR) | GO(PIN_RD))
 #define GPFSEL1_OUTPUT (GO(PIN_D(11)) | GO(PIN_D(10)) | GO(PIN_D(9)) | GO(PIN_D(8)) | GO(PIN_D(7)) | GO(PIN_D(6)) | GO(PIN_D(5)) | GO(PIN_D(4)) | GO(PIN_D(3)) | GO(PIN_D(2)))
-#define GPFSEL2_OUTPUT (GO(PIN_A(2)) | GO(PIN_A(1)) | GO(PIN_A(0)) | GO(PIN_D(15)) | GO(PIN_D(14)) | GO(PIN_D(13)) | GO(PIN_D(12)) | PF(28,5) | PF(29,5))
+#define GPFSEL2_OUTPUT (GO(PIN_A(2)) | GO(PIN_A(1)) | GO(PIN_A(0)) | GO(PIN_D(15)) | GO(PIN_D(14)) | GO(PIN_D(13)) | GO(PIN_D(12)))
 
 #define REG_DATA_LO     0
 #define REG_DATA_HI     1
